2e9043ca0dd39494338d8793d6f08d8a81ef4eaa
Yasindi98 <amasenadeera98@gmail.com>
2025-01-16 11:27:50 +0530

    Q4 is created.

---
+++ src/main/java/com/stramandlamda/Q4.java
@@ -0,0 +1,11 @@
+package com.stramandlamda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Q4 {
+    public static void main(String[] args) {
+        List<String> nameList = Arrays.asList("Ama", "Kalana", "John");
+        nameList.forEach(System.out::println);
+    }
+}
